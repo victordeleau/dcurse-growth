f3f36b06ad9c4becb70ff43bbbe91446ff74ef02
Victor Deleau <victor.deleau@gmx.com>
2020-06-04 10:35:28 +0200

    add readme.md

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # What is the sparsity growth rate of random points in a hypercube as its dimensionality increases ?
 
-Or which number lies behind the famous curse of dimensionality in machine learning.
+Exact solutions are known for the square and the cube (whose average distance between random points is known as Robbins constant = 0.6617..). For d-dimensional hypercubes, those distances can be estimated by Monte Carlo experiments.
+
+This begs the question: how fast is this average distance growing? In machine learning, the curse of dimensionality informs us that easy to compute solutions in low dimensional space will quickly become computationally prohibitive as the dimensionality of the input space increases. Data points are usually considered to be i.i.d. In that case, if we add a dimension to our input space, how much more data points, on average, do we need for those data points to be as far away from each other as before?
+
+The average distance between random points is computed up to a specific number of dimensions. A quick look at the data indicates that the growth rate is slowing as the dimensionality increases. Therefore, we take the log base e of every value on the X axis, and do a regular linear regression using the vectorized closed-form solution. We obtained a log curve with the following equation:
+
+**y = 1.11 ln(x) - 1.22**
+
+We used 1000000 samples per hypercube and dimensions from 1 to 128. The coefficient of determination R² was equal to 0.999673
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use rand::{Rng, thread_rng};
 
 use ndarray::*;
 use ndarray_linalg::*;
-use ndarray::prelude::*;
 
 use std::env;
 
@@ -11,34 +10,34 @@ fn main() {
     ////////////////////////////////////////////////////////////////////////////
     // random uniform sampling of d-dimensional hypercube
 
-    let args: Vec<String> = env::args().collect();
+    let _args: Vec<String> = env::args().collect();
 
-    let samples = 1024;
-    let dimensions = 512;
+    let samples = 1000000;
+    let dimensions = 128;
 
-    let mut X = Array::ones((dimensions, 2).f());
-    let mut Y = Array1::<f64>::zeros(dimensions);
+    let mut x = Array::ones((dimensions, 2).f());
+    let mut y = Array1::<f64>::zeros(dimensions);
 
     let mut tmp: f64 = 0.;
     let mut current_rmse: f64 = 0.;
-    let mut p1: f64;
-    let mut p2: f64;
 
     let mut die = thread_rng();
 
     for d in 1..dimensions {
 
-        for i in 0..samples {
+        for _i in 0..samples {
 
-            for j in 0..d {
+            for _j in 0..d {
                 tmp += (die.gen_range(0., 1.)-die.gen_range(0., 1.)).powi(2);
             }
             current_rmse += tmp.powf(0.5);
             tmp = 0.;
         }
 
-        X[[d, 0]] = (d as f64).ln();
-        Y[d] = current_rmse/(samples as f64);
+        x[[d, 0]] = (d as f64).ln();
+        y[d] = current_rmse/(samples as f64);
+
+        print!("X = {}, Y = {:.3}\n", d, y[d]);
 
         current_rmse = 0.;
     }
@@ -48,17 +47,17 @@ fn main() {
     // fit logarithmic curve
 
     // closed form solution
-    let ab = (X.t().dot(&X)).inv().unwrap().dot(&X.t()).dot(&Y); 
+    let ab = (x.t().dot(&x)).inv().unwrap().dot(&x.t()).dot(&y); 
     let a = ab[0];
     let b = ab[1];
 
     // compute goodness of fit using R^2 value
-    let mean_y: f64 = Y.iter().sum();
+    let mean_y: f64 = y.iter().sum();
     let mut u: f64 = 0.;
     let mut l: f64 = 0.;
     for i in 0..(dimensions as usize) {
-        u += (Y[i]-(a+(b*X[[i, 0]]))).powf(2.);
-        l += (Y[i]-mean_y).powf(2.);
+        u += (y[i]-(a+(b*x[[i, 0]]))).powf(2.);
+        l += (y[i]-mean_y).powf(2.);
     }
     let r_squared = 1. - (u/l);
 
